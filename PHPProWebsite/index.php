1927e5a7ef652f8eac1a44c50458cf065a238cd1
Maurice Hoekstra <maurice.hoekstra@student.nhlstenden.com>
2018-10-11 10:07:28 +0200

    Added PHP Project
    PHP Project added

---
+++ PHPProWebsite/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Welkom</title>
+    </head>
+    <body>
+        <?php
+        /*
+         * Filename   :   index.php
+         * Assignment :   Professional Website
+         * Created    :   11-10-2018
+         * Description:   Professional Website with php
+         * Programmers :   Maurice Hoekstra, Tim Groenewold, Kevin Trip, Onno Busscher,
+         *                 Felice Zirkzee, Robin Haan, Jurrian Tanke
+         */
+        ?>
+    </body>
+</html>
