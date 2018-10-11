1c6919683da0e95d8fa18689fa2a137adef99e00
Maurice Hoekstra <maurice.hoekstra@student.nhlstenden.com>
2018-10-11 10:56:09 +0200

    Update PHPProWebsite/RSSFeed.php
    RSS Feed script

---
+++ PHPProWebsite/RSSFeed.php
@@ -0,0 +1,23 @@
+<?php
+/*
+ * Filename   :   index.php
+ * Assignment :   Professional Website RSS Feed
+ * Created    :   11-10-2018
+ * Description:   Professional Website RSS Feed functions
+ * Programmers :   Maurice Hoekstra
+ */
+?>
+
+
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>RSS FEED</title>
+    </head>
+    <body>
+
+    </body>
+</html>
+
