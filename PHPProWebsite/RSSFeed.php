0f57c9d5b341cc12a4d81f867247f43b0e13e0ed
Maurice Hoekstra <maurice.hoekstra@student.nhlstenden.com>
2018-10-12 10:31:57 +0200

    Update PHPProWebsite/RSSFeed.php
    Made RSS Feed function actually functional

--- PHPProWebsite/RSSFeed.php
+++ PHPProWebsite/RSSFeed.php
@@ -1,11 +1,29 @@
 <?php
 /*
- * Filename   :   index.php
+ * Filename   :   RSSFeed.php
  * Assignment :   Professional Website RSS Feed
  * Created    :   11-10-2018
  * Description:   Professional Website RSS Feed functions
  * Programmers :   Maurice Hoekstra
  */
+
+    function rssLoader($url, $startDataNum, $endDataNum){
+        //Loads RSS Feed through simplexml, returns the feed.
+        $html = "";
+        $xml = simplexml_load_file($url);
+        for($i = $startDataNum; $i < $endDataNum; $i++){
+            //Arrow is Object method to access properties of an object
+            $title = $xml->channel->item[$i]->title;
+            $link = $xml->channel->item[$i]->link;
+            $description = $xml->channel->item[$i]->description;
+            $pubDate = $xml->channel->item[$i]->pubDate;
+
+                $html .= "<a href='$link'><h3>$title</h3></a>";
+            $html .= "$description";
+            $html .= "<br />$pubDate<hr />";
+        }
+        return $html;
+    }
 ?>
 
 
@@ -16,7 +34,7 @@
         <meta charset="UTF-8">
         <title>RSS FEED</title>
     </head>
-    <body>
+    <body> 
         
     </body>
 </html>
