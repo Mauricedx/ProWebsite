6b3a86a133b3ef2645443007bba4e4cb1f58a37e
Maurice Hoekstra <maurice.hoekstra@student.nhlstenden.com>
2018-10-17 13:31:28 +0200

    Further edited index and styled, resized planeimg

--- PHPProWebsite/index.php
+++ PHPProWebsite/index.php
@@ -8,13 +8,28 @@
     <body>
         <header>
             <h1>Vleugels Hogeschool</h1>
-            <p>- Muziekbouw en vliegtuigbouw -</p>
+            <p>- Muziek en vliegtuigbouw -</p>
         </header>
         <div id="navbar">
             <ul>
-                <li><a href="html/Informatie.html">over</a></li>
-                <li><p>Dropdowntest</p>
+                <li><a href="html/Informatie.html">Over</a></li>
+                <li><div class="dropdownnav"><p>Opleidingen</p>
+                        <div  class="navddcontent">
+                            <a href="html/KlassiekeMuziekInstrumenten.html">Klassieke Intrumentenbouw</a><br/>
+                            <a href="html/Vliegtuigbouw.html">Vliegtuigbouw</a><br/>
+                            <a href="html/Helikopterbouw.html">Helikopterbouw</a><br/>
+                            <a href="html/ElektronischeMuziek.html">Electronische Muziek</a><br/>
+                        </div>
+                    </div>
+                </li>
+                <li><a href="https://student.sl-cloud.nl/">Inschriven</a></li>
+                <li><p>Welkom bij Vleugels Hogeschool!</p></li>
+                <li><a href="html/Contact.html">Contact</a></li>
+                <li><a href="#">Inloggen</a></li>
+                <li><a href="#">Foto's</a></li>
             </ul>
+        </div>
+        <div id="planeimg">    
         </div>
         <div id ="content">
         <?php
