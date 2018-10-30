c39bc04e9759e190b5bec4901db1b8b17b31062c
Maurice Hoekstra <maurice.hoekstra@student.nhlstenden.com>
2018-10-30 12:29:13 +0100

    Updated upload checker and gitignore

--- PHPProWebsite/php/photocheck.php
+++ PHPProWebsite/php/photocheck.php
@@ -31,23 +31,24 @@
                 $photoDir = '../upphoto/';
                 $photoArray = scandir($photoDir);
                 $varPhotoDirCount = count($photoArray); 
+                echo '<form action="' . htmlentities($_SERVER['PHP_SELF']). '" method=POST>';
+                echo '<div id="photocheckform"><input type="submit" name="approve" value="Goedkeuren">';
+                echo '<input type="submit" name="delete" value="Delete"></div>';
                 for($count = 3; $count <= $varPhotoDirCount -1; $count++){
                     //Echo to be approved photos in a form with a checkbox, depending on which submit button, the checked photos will be approved or deleted
-                    echo '<div class="photocheckform"><form action="' . htmlentities($_SERVER['PHP_SELF']). '" method=POST>';
+                    echo '<div class="photostobejudged">';
                     echo '<img src="../upphoto/'.  $photoArray[$count] . '" alt="UploadedPhoto" height="200" width="200"/></br>'; 
-                    echo '<input type = checkbox name= photoselect[] value = "../upphoto/' . $photoArray[$count] . '"/>';
-                    
+                    echo '<input type="checkbox" name="photoselect[]" value="../upphoto/' . $photoArray[$count] . '"/>';
+                    echo '</div>';
                     
                 }
-                echo '<input type= submit name = approved value = "Goedkeuren">';
-                echo '<input type= submit name = delete value = "Delete">';
-                echo '</form></div>';
+                echo '</form>';
                 //If submit button approve is pressed, and it's checkbox array is not empty,move all selected photos to approved photo folder
-                if (isset($_POST["approved"])) {
+                if (isset($_POST["approve"])) {
                     if (isset($_POST['photoselect'])){
-                        foreach($_POST['photoselect'] as $photo){
-                            $newdir = '../upphotoapproved' . substr($photo, 10);
-                            rename($photo, $newdir);
+                        foreach($_POST['photoselect'] as $oldDir){
+                            $newDir = '../upphotoapproved' . substr($oldDir, 10);
+                            rename($oldDir, $newDir);
                         }
                     }
                 }
